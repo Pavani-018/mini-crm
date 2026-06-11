1c868ffa6f5b99a3bbe9658a98ce0c865fabefaa
Pavani <your-email@gmail.com>
2026-06-11 18:53:57 +0530

    fix render port

--- server/server.js
+++ server/server.js
@@ -69,8 +69,10 @@ app.post("/leads", (req, res) => {
   );
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 app.delete("/leads/:id", (req, res) => {
   const id = req.params.id;
